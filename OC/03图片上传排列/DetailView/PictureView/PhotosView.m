c4b96c6d12531ccd0368cf785afaf21826168431
hundredb <wanghj004@qianhai.com.cn>
2017-03-16 14:19:13 +0800

    基本实现添加功能

--- OC/03图片上传排列/DetailView/PictureView/PhotosView.m
+++ OC/03图片上传排列/DetailView/PictureView/PhotosView.m
@@ -31,17 +31,22 @@
 #pragma mark - setter / getter
 
 
+-(void)setPicArr:(NSMutableArray *)picArr
+{
+    _picArr = picArr;
+    NSLog(@"输入的图片");
+}
 
 -(NSMutableArray *)picArr
 {
     if (!_picArr) {
         _picArr = [@[] mutableCopy];
     }
-    if (![_picArr containsObject:_addBtn]) {
-        //进行btn的排序
-        [_picArr addObject:_addBtn];
-        [_picArr addObject:_cancelBtn];
-    }
+//    if (![_picArr containsObject:_addBtn]) {
+//        //进行btn的排序
+//        [_picArr addObject:_addBtn];
+//        [_picArr addObject:_cancelBtn];
+//    }
     return _picArr;
 }
 
@@ -76,11 +81,12 @@
     [[addbtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
         UIButton *btn = [UIButton new];
         [self.picArr addObject:btn];
+        btn.backgroundColor = H_UI_RandomColor;
         [self addSubview:btn];
         NSLog(@"点击了添加按钮");
         
     }];
-//    [self addSubview:addbtn];
+    [self addSubview:addbtn];
     _addBtn = addbtn;
     
     //删除按钮
@@ -99,7 +105,7 @@
         }
         NSLog(@"点击了删除按钮");
     }];
-//    [self addSubview:cancelBtn];
+    [self addSubview:cancelBtn];
     _cancelBtn = cancelBtn;
     
     
@@ -114,19 +120,23 @@
 {
     [super layoutSubviews];
     
+    CGRect frameRect = self.frame;
     CGFloat lastBtnMaxX = 0 ;
-    UIButton *lastBtn ;
 
-    
+    //复制多一个临时的数组主要为了添加/删除按钮
+    NSMutableArray *tmpArr = [self.picArr mutableCopy];
+    [tmpArr addObject:_addBtn];
+    [tmpArr addObject:_cancelBtn];
     
     //获取添加的数组数目
-    long count = self.picArr.count;
+    long count = tmpArr.count;
     int maxColumns = 4; // 一行最多显示4张图片
     CGFloat btnW = PHOTOWH;
     CGFloat btnH = btnW;
 
     CGFloat magin = (CGRectGetWidth(self.frame)-maxColumns*btnW)/(maxColumns+1);
-    
+
+#if 0
     for (int i = 0; i<count; i++) {
         NSInteger row = i / maxColumns;
         NSInteger column = i %maxColumns;
@@ -135,6 +145,7 @@
         CGFloat btnX = magin + (magin+btnW)*column;
         CGFloat btnY = magin + (magin+btnW)*row;
         
+
         if (i == count-1) {
             [btn mas_remakeConstraints:^(MASConstraintMaker *make) {
                 make.top.equalTo(self).offset(btnY);
@@ -149,27 +160,40 @@
             make.left.equalTo(self).offset(btnX);
             make.width.and.height.mas_equalTo(btnW);
         }];
-
-        
         lastBtn = btn;
         /*
-        CGFloat btnX = magin + (magin+btnW)*column;
-        CGFloat btnY = magin + (magin+btnW)*row;
-        btn.frame = CGRectMake(btnX, btnY, btnW, btnH);
-        if (i==count-1) {
-            lastBtnMaxX = CGRectGetMaxX(btn.frame);
-        }
+         CGFloat btnX = magin + (magin+btnW)*column;
+         CGFloat btnY = magin + (magin+btnW)*row;
+         btn.frame = CGRectMake(btnX, btnY, btnW, btnH);
+         if (i==count-1) {
+         lastBtnMaxX = CGRectGetMaxX(btn.frame);
+         }
          */
     }
+#else
     
-    /*
-    //放置添加按钮
-    _addBtn.frame = (CGRect){lastBtnMaxX+PHOTOMARGIN,(CGRectGetHeight(self.frame)-PHOTOWH)/2.0,PHOTOWH,PHOTOWH};
-    //放置减少按钮
-    _cancelBtn.frame = (CGRect){lastBtnMaxX+PHOTOWH+PHOTOMARGIN*2,btnY,PHOTOWH,PHOTOWH};
+    
+    for (int i = 0; i<count; i++) {
+        NSInteger row = i / maxColumns;
+        NSInteger column = i %maxColumns;
+        NSLog(@"打印的行%d和列%d,还有总数%d",row,column,count);
+        UIButton *btn = tmpArr[i];
+        CGFloat btnX = magin + (magin+btnW)*column;
+        CGFloat btnY = magin + (magin+btnW)*row;
+        btn.frame = (CGRect){btnX,btnY,PHOTOWH,PHOTOWH};
+        lastBtnMaxX = CGRectGetMaxX(btn.frame);
+    }
+//    //放置添加按钮
+//    _addBtn.frame = (CGRect){lastBtnMaxX+PHOTOMARGIN,PHOTOMARGIN,PHOTOWH,PHOTOWH};
+//    //放置减少按钮
+//    _cancelBtn.frame = (CGRect){lastBtnMaxX+PHOTOWH+PHOTOMARGIN*2,PHOTOMARGIN,PHOTOWH,PHOTOWH};
     //这里编辑添加后的的尺寸
-    self.contentSize= (CGSize){CGRectGetMaxX( _cancelBtn.frame)+magin,self.frame.size.height};
-     */
+//    self.contentSize= (CGSize){CGRectGetMaxX( _cancelBtn.frame)+magin,self.frame.size.height};
+    frameRect.size.height = CGRectGetMaxY(_cancelBtn.frame)+magin;
+    self.frame = frameRect;
+#endif
+        
+        
 }
 
 
--- OC/03图片上传排列/DetailView/PictureViewController.m
+++ OC/03图片上传排列/DetailView/PictureViewController.m
@@ -21,11 +21,13 @@
     self.view.backgroundColor = [UIColor yellowColor];
     
     PhotosView *pic = [PhotosView new];
+    pic.backgroundColor = [UIColor redColor];
     [self.view addSubview:pic];
     [pic mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view).offset(100);
         make.centerX.equalTo(self.view);
         make.width.equalTo(self.view);
+//        make.height.greaterThanOrEqualTo(@100);
     }];
     
     
